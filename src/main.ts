1d087a33704e2be6c2cf5ef6d51b5fb736ca50de
MADStitch-secretlab <limdonghyuk12@gmail.com>
2025-11-01 17:33:04 +0900

    second

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidUnknownValues: false,
+  }));
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5000',
+      'https://port-next-gdgoc-game-front-m4h5z3nf13944b7f.sel4.cloudtype.app'
+
+    ]
+  });
+  await app.listen(5000);
 }
 bootstrap();
